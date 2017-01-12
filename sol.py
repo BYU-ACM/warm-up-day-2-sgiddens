98e6f9d4949025caf7815af97bb4c15195762908
Spencer Giddens <giddens2spencer@gmail.com>
2017-01-12 12:32:48 -0700

    added file

--- sol.py
+++ sol.py
@@ -1,16 +1,33 @@
 def Binary_Search(arr, to_find):
-  """A direct implementation of Newton's Method
+    """A direct implementation of Newton's Method
      (For sanity assume that func and func_prime define there own division correctly,
      that is, don't cast anything to a float)
      params: arr (a list ordered from least to greatest)
              to_find (the item to find in arr)
      returns: index (int), x, s.t. arr[x] == to_find
               None(none-type) if for all x, arr[x] != to_find
-  """
-  pass
+    """
+    x = len(arr) / 2 
+    found = True
+    while arr[x] != to_find:
+
+        if len(arr) == 1 and arr[x] != to_find:
+            found = False
+            break
+        else:
+            if to_find > arr[x]:
+                arr = arr[x:]
+            elif to_find < arr[x]:
+                arr = arr[:x]
+            else:
+                return x
+            x = len(arr) / 2
+    if not found:
+        return None
+
 
 def Bisection(func, left_side, right_side, tol=1e-5):
-  """A direct implementation of Newton's Method
+    """A direct implementation of Newton's Method
      (For sanity assume that func and func_prime define there own division correctly,
      that is, don't cast anything to a float)
      params: func (a function)
@@ -19,5 +36,5 @@ def Bisection(func, left_side, right_side, tol=1e-5):
              tol (a value for which the function should return once a value at least that distance from zero is found)
      returns: root (float), x, s.t. abs(func(x))<tol
               None(none-type) if func(left_side), func(right_side) < 0 or func(left_side), func(right_side) > 0
-  """
-  pass
+    """
+    pass
